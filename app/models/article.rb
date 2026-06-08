13e677210c17c5e6bb8efaee7f3b368b9e455dcd
Raxa27 <huhhhdevil@gmail.com>
2026-06-08 20:00:50 +0500

    Add one-to-many association between users and articles from back end

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :description, presence: true
+  belongs_to :user
+  validates :title, presence: true
+  validates :description, presence: true
 end
