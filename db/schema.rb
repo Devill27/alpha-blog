773450f7375997e94c03cc869d228a45b4b79cc8
Raxa27 <huhhhdevil@gmail.com>
2026-06-09 16:52:18 +0500

    Add new users signup form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_08_130503) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_09_103922) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at"
     t.text "description"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_08_130503) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
+    t.string "password_digest"
     t.datetime "updated_at", null: false
     t.string "username"
   end
